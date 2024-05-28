6ad10c24c707d71c02ab7d6230c97f252f900914
Akshat <82810411+Afterdie@users.noreply.github.com>
2024-05-29 02:08:25 +0530

    working menu

---
+++ app/menu/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React, { useState } from "react";
+import data from "../../../menu.json";
+import ProductButton from "../comps/ProductButton";
+import { ButtonType } from "../comps/ProductButton";
+
+interface Item {
+  id: string;
+  count: number;
+  large: boolean;
+  boba: boolean;
+}
+
+interface Order {
+  item: Item[];
+}
+
+export default function page() {
+  const [order, setOrder] = useState<Item[]>([]);
+
+  const onButtonClickGeneric = (id: string, buttonType: ButtonType): void => {
+    console.log(order);
+    //all three button lead here therefore the logic
+    setOrder((prevOrder) => {
+      const existingItem = prevOrder.findIndex(
+        (orderItem) => orderItem.id === id
+      );
+      console.log(existingItem);
+      if (buttonType === ButtonType.delete)
+        return prevOrder.filter((item) => item.id !== id);
+
+      if (existingItem !== -1) {
+        return prevOrder.map((orderItem, index) =>
+          index === existingItem
+            ? {
+                ...orderItem,
+                count:
+                  buttonType === ButtonType.count
+                    ? orderItem.count + 1
+                    : orderItem.count,
+                large:
+                  buttonType === ButtonType.large
+                    ? !orderItem.large
+                    : orderItem.large,
+                boba:
+                  buttonType === ButtonType.boba
+                    ? !orderItem.boba
+                    : orderItem.boba,
+              }
+            : orderItem
+        );
+      } else {
+        const newEntry = {
+          id: id,
+          count: 1,
+          large: buttonType === ButtonType.large ? true : false,
+          boba: buttonType === ButtonType.boba ? true : false,
+        };
+        return [...prevOrder, newEntry];
+      }
+    });
+  };
+  return (
+    <div className="grid md:grid-cols-6 grid-cols-2 gap-4">
+      {data.map((item, index) => {
+        const orderItem = order.find((orderItem) => orderItem.id === item.name);
+        return (
+          <ProductButton
+            productDetails={{ id: item.name, large: item.large }}
+            orderDetails={{
+              count: orderItem?.count || 0,
+              large: orderItem?.large || false,
+              boba: orderItem?.boba || false,
+            }}
+            onButtonClickGeneric={onButtonClickGeneric}
+            key={index}
+          ></ProductButton>
+        );
+      })}
+    </div>
+  );
+}
